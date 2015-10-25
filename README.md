f9c54e5421684c94c79809268b0bfc410bb8a9d1
dylanjharris <dylansbook@gmail.com>
2015-10-25 13:12:23 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Welcome
+=======
+
+This is a custom WordPress theme for Marchese Law Group. You can see it in action at&hellip;
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Marchese
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_mlg' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_mlg' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_mlg' ), '_mlg', '<a href="https://dylanjharris.net" rel="designer">Dylan J Harris</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
