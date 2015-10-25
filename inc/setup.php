6ed523994bf663aaa9871787d4bf50422d2c9030
dylanjharris <dylansbook@gmail.com>
2015-10-25 14:39:21 -0700

    Colorsteez and adding social menu

--- inc/setup.php
+++ inc/setup.php
@@ -45,6 +45,7 @@ function _mlg_setup() {
   // This theme uses wp_nav_menu() in one location.
   register_nav_menus( array(
     'primary' => esc_html__( 'Primary Menu', '_mlg' ),
+    'social' => esc_html__( 'Social Menu', '_mlg' ),
   ) );
 
   /*
