16d72be2e696def2c6a12fdbdb23076992314583
dylanjharris <dylansbook@gmail.com>
2015-10-26 13:20:33 -0700

    Updating

--- header.php
+++ header.php
@@ -25,23 +25,26 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', '_mlg' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<img class="site-logo" src="wp-content/themes/_mlg/img/logo.png" title="Marchese Law Group" />
-			<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-<!-- <p class="site-description"><?php // bloginfo( 'description' ); ?></p> -->
-			<div class="site-description">
-				<div><span>C</span>ENTRAL<span> C</span>OAST</div>
-				<div><span>B</span>ANKRUPTCY<span> L</span>AWYERS</div>
-			</div>
-		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', '_mlg' ); ?></button>
+			
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 
 			<?php _mlg_social_menu(); ?>
-
+		
 		</nav><!-- #site-navigation -->
+
+		<div class="site-branding">
+			<img class="site-logo" src="wp-content/themes/_mlg/img/logo.png" title="Marchese Law Group" />
+			<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h2 class="site-description">
+				<div><span>C</span>ENTRAL<span> C</span>OAST</div>
+				<div><span>B</span>ANKRUPTCY<span> L</span>AWYERS</div>
+			</h2>
+		</div><!-- .site-branding -->
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
