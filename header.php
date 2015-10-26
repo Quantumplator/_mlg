ff128e81a6cefb205126c49660869609d72e8310
dylanjharris <dylansbook@gmail.com>
2015-10-26 10:48:47 -0700

    Updating

--- header.php
+++ header.php
@@ -26,11 +26,8 @@
 
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
-			<?php if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php endif; ?>
+			<img src="wp-content/themes/_mlg/img/logo.png" title="Marchese Law Group" class="logo" />
+			<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<p class="site-description"><?php bloginfo( 'description' ); ?></p>
 		</div><!-- .site-branding -->
 
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,6 +6,8 @@
 function _mlg_scripts() {
   wp_enqueue_style( '_mlg-style', get_stylesheet_uri() );
 
+  wp_enqueue_style( '_mlg-fonts', 'https://fonts.googleapis.com/css?family=Lora:700,400|Source+Sans+Pro:700,400italic,400' );
+
   wp_enqueue_script( '_mlg-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
   wp_enqueue_script( '_mlg-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
