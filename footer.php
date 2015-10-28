c5555b4fb08190679b42a72002d11445f6e522c1
dylanjharris <dylansbook@gmail.com>
2015-10-28 16:07:06 -0700

    Updating

--- footer.php
+++ footer.php
@@ -15,13 +15,19 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_mlg' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_mlg' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_mlg' ), '_mlg', '<a href="https://dylanjharris.net" rel="designer">Dylan J Harris</a>' ); ?>
+			<div id="disclaimer" class="site-disclaimer"><strong>Disclaimer:</strong> The information you obtain at this site is not, nor is it intended to be, legal advice. You should consult an attorney for advice regarding your individual situation. We invite you to contact our office and we welcome your calls, letters and electronic mail. Contacting us does not create an attorney-client relationship. Please do not send any confidential information to us until such time as an attorney-client relationship has been established. Marchese Law Group is a debt relief agency. We help people file for bankruptcy relief under the Bankruptcy Code.</div>
+			<div id="footer-address" class="site-address">
+				<ul>
+					<li>FAX: (805) 541-5412</li>
+					<li>873 A Santa Rosa Street</li>
+					<li>San Luis Obispo, CA 93401</li>
+					<li><?php _mlg_social_menu(); ?></li>
+				</ul>
+			</div>
 		</div><!-- .site-info -->
-
-    <?php // _mlg_social_menu(); ?>
-
+		<div id="copyright" class="site-copyright">
+			Copyright &copy; <?php echo date('Y'); ?> Marchese Law Group
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
