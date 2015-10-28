452532978545de0715c540f7c148984fb444fce2
dylanjharris <dylansbook@gmail.com>
2015-10-28 11:10:15 -0700

    Updating

--- header.php
+++ header.php
@@ -62,10 +62,9 @@
 
 	<div id="content" class="site-content">
 		<div id="hero" class="splash-header">
-			<div>
-				<h1><span class="needa">need a</span> <span class="freshstart">FRESH START?</span></h1>
-				<p><strong>Call Marchese Law Group today!</strong> At your free initial consultation, we will take the time to understand your financial situation, explain what types of Bankruptcy protection are available, and establish a plan to meet your individual needs.  No hassle, no hidden costs and no pressure; just honest, professional, legal advise.</p>
-			</div>
+			<h1><span class="needa">need a</span> <span class="freshstart">FRESH START?</span></h1>
 			<img src="http://localhost:8888/marchese/wp-content/uploads/marcheses.png" title="Marchese Law Group" />
+			<p><strong>Call Marchese Law Group today!</strong> At your free initial consultation, we will take the time to understand your financial situation, explain what types of Bankruptcy protection are available, and establish a plan to meet your individual needs.  No hassle, no hidden costs and no pressure; just honest, professional, legal advise.</p>
+
 		</div>
 		<div id="below-splash" class="content-container">
